762a958a603fcc250c6d9b4ed18d9c7ff6e38479
magnus-frid-nti-johanneberg <magnus.frid@elev.ga.ntig.se>
2022-03-07 10:03:15 +0100

    2022.03.07

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// Denna fil skall innehålla JavaScript (JS)
+function toggleMeny(){
+
+    document.querySelector('.navLinks').classList.toggle('navActive')
+    document.querySelector('.burger').classList.toggle('toggle')
+
+}
+
+document.querySelector('.burger').addEventListener('click', toggleMeny)
