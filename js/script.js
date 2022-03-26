5a1ff699c2b77d28322830d58fa27f117ebbb76c
magnus-frid-nti-johanneberg <magnus.frid@elev.ga.ntig.se>
2022-03-26 17:20:28 +0100

    2022.03.26 contact-form

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function toggleMeny(){
 
     document.querySelector('.navLinks').classList.toggle('navActive')
     document.querySelector('.burger').classList.toggle('toggle')
-
 }
 
 document.querySelector('.burger').addEventListener('click', toggleMeny)
+
